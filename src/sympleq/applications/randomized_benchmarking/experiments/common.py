9f7bc7cce6029169ac2471373cbf6e768b713127
Shreya Banerjee <shreya93ban@gmail.com>
2026-09-15 16:00:10 +0100

    Charlie_20268_depol

--- src/sympleq/applications/randomized_benchmarking/experiments/common.py
+++ src/sympleq/applications/randomized_benchmarking/experiments/common.py
@@ -36,8 +36,8 @@ from sympleq.integrations.quantinuum.utils import (
 from sympleq.applications.randomized_benchmarking.backends.sympleq import SympleqBackend
 from sympleq.core.noise.noise_model import DephasingNoise, GenericNoise
 
-ONE_Q_DEPHASING = 0.000167
-TWO_Q_DEPHASING = 0.00245
+ONE_Q_DEPHASING = 0.000199
+TWO_Q_DEPHASING = 0.00226
 
 def dephasing_sympleq_backend_factory(settings, rng):
     return SympleqBackend(
